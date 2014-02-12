6d3e922160d0ab14701b9941e8e7de49e2335aa7
Daniel Evans <danharibo@gmail.com>
2014-02-12 18:06:43 +0000

    Minor improvements

--- clock.js
+++ clock.js
@@ -1,3 +1,28 @@
+(function() {
+    var lastTime = 0;
+    var vendors = ['webkit', 'moz'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame =
+          window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
 function ClockHand(unit) {
 	var rot = $('<div class="clock-rot '+unit+'">');
 	var hand = $('<div class="clock-hand '+unit+'">');
@@ -30,7 +55,6 @@ function getClock(tz, offset) {
 	clocks.push(c);
 	return box;
 }
-console.log((new Date()).getUTCHours());
 
 function tickClocks() {
 	var D = new Date();
@@ -45,9 +69,11 @@ function tickClocks() {
 			tseconds += (d) + (Math.sin((d/(1-secondhandticktime))*1.57075) * 1.5);
 		}
 		tseconds /= 60;
-		clocks[c].hours.css('-webkit-transform', 'rotate('+(-90+(thours)*360)+'deg)');	
-		clocks[c].minutes.css('-webkit-transform', 'rotate('+(-90+(tmin)*360)+'deg)');	
-		clocks[c].seconds.css('-webkit-transform', 'rotate('+(-90+(tseconds)*360)+'deg)');	
+		['', '-webkit-', '-moz-'].forEach(function(ua){
+			clocks[c].hours.css(ua+'transform', 'rotate('+(-90+(thours)*360)+'deg)');	
+			clocks[c].minutes.css(ua+'transform', 'rotate('+(-90+(tmin)*360)+'deg)');	
+			clocks[c].seconds.css(ua+'transform', 'rotate('+(-90+(tseconds)*360)+'deg)');	
+		});
 	}
 	requestAnimationFrame(tickClocks);
 }
